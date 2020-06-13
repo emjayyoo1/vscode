c8f39949747559c1ffb0783fc6f140b7c552764a
Jack Lake <jacklake@Jacks-MacBook-Pro.local>
2020-06-13 20:34:58 +0100

    update

--- poker.py
+++ poker.py
@@ -83,6 +83,7 @@ class Player(object):
         self.card1 = card1
         self.in_hand = True
         self.flush = False
+        self.flush_hand = []
         self.made_hand = []
         self.made_hand_three = []
         self.made_hand_four = []
@@ -92,7 +93,11 @@ class Player(object):
         self.four_of_kind = False
         self.straight = False
         self.full_house = False
+        self.full_house_test = []
+        self.is_flush = False
+        self.is_straight_flush = False
         self.straight_list = []
+        self.score = 0
 
     # Functions
 
@@ -109,39 +114,17 @@ class Player(object):
         deck.cards.append(self.card1.pop())
         print(self.firstname, 'hand cleared\n')
 
-    # Check hand ranking
-
-    # Flush
-
-    def flush_suit_checker(self):
-        self.seven_cards = self.card1 + game.flop
-        c = Counter(getattr(x, 'suit') for x in self.seven_cards)
-        if (c['Clubs'] or c['Hearts'] or c['Diamonds'] or c['Spade']) >= 5:
-            print(self.firstname, ': flush')
-            self.flush = True
-
-        else:
-            pass
-
-        clubs = sum(1 for x in self.seven_cards if x.suit == "Clubs")
-        hearts = sum(1 for x in self.seven_cards if x.suit == "Hearts")
-        diamonds = sum(1 for x in self.seven_cards if x.suit == "Diamonds")
-        spades = sum(1 for x in self.seven_cards if x.suit == "Spades")
-
-        if self.flush == True:
-            if clubs >= 5:
-                print('Clubs')
-            if hearts >= 5:
-                print('Hearts')
-            if diamonds >= 5:
-                print('Diamonds')
-            if spades >= 5:
-                print('Spades')
-
-    # Hand ranking 4 of a kind - pair
+    # ========================= Hand ranking all ============================
 
     def hand_ranking(self):
+
         self.seven_cards = self.card1 + game.flop
+        # Pair, three, four of a kind
+
+        self.test2 = []
+        for x in self.seven_cards:
+            self.test2.append(x.value)
+
         c = Counter(getattr(x, 'value') for x in self.seven_cards)
         for x in c:
 
@@ -152,42 +135,33 @@ class Player(object):
             if c[x] == 3:
                 self.made_hand_three.append(x)
                 self.three_of_kind = True
+                self.full_house_test.append(x)
+                self.full_house_test.append(x)
+                self.full_house_test.append(x)
 
             if self.pair == 0:
                 if c[x] == 2:
                     self.made_hand.append(x)
+                    self.made_hand.append(x)
                     self.pair += 1
+                    self.full_house_test.append(x)
+                    self.full_house_test.append(x)
+
             else:
                 if c[x] == 2:
+                    self.made_hand.append(x)
                     self.made_hand.append(x)
                     self.two_pair = True
                     self.pair += 1
 
-        if self.four_of_kind == True:
-            print(self.firstname, 'Four of a kind : ', self.made_hand_four)
+        # Straight
 
-        elif (self.three_of_kind and self.pair) == True:
-            print(self.firstname, 'full house : ', self.made_hand_three,
-                  "'s over ", self.made_hand, "'s")
-
-        elif self.three_of_kind == True:
-            print(self.firstname, 'Three of a kind : ', self.made_hand_three)
-
-        elif self.pair >= 2:
-            print(self.firstname, 'Two pairs of : ', self.made_hand)
-
-        elif self.pair == 1:
-            print(self.firstname, 'Pairs of : ', self.made_hand)
-
-    # Hand ranking for straight
-
-    def hand_ranking_straight(self):
-        self.seven_cards = self.card1 + game.flop
-        c = sorted(self.seven_cards, key=attrgetter('value'), reverse=True)
+        d = sorted(self.seven_cards, key=attrgetter('value'), reverse=True)
         count_straight = 0
-        for x in c:
 
-          #[6,7,8,9,J,A ]
+        for x in d:
+
+          # [6,7,8,9,J,A ]
 
             if count_straight == 0:
                 self.straight_list.append(x)
@@ -209,57 +183,175 @@ class Player(object):
                     self.straight_list.insert(0, x)
                     count_straight = 1
 
+        # Flush
+
+        e = Counter(getattr(x, 'suit') for x in self.seven_cards)
+
+        if (e['Clubs'] or e['Hearts'] or e['Diamonds'] or e['Spade']) >= 5:
+            print(self.firstname, ': flush')
+            self.flush = True
+
+        else:
+            pass
+
+        clubs = sum(1 for x in self.seven_cards if x.suit == "Clubs")
+        hearts = sum(1 for x in self.seven_cards if x.suit == "Hearts")
+        diamonds = sum(1 for x in self.seven_cards if x.suit == "Diamonds")
+        spades = sum(1 for x in self.seven_cards if x.suit == "Spades")
+
+        if self.flush == True:
+
+            if clubs >= 5:
+                suit_return = 'Clubs'
+            if hearts >= 5:
+                suit_return = 'Hearts'
+            if diamonds >= 5:
+                suit_return = 'Diamonds'
+            if spades >= 5:
+                suit_return = 'Spades'
+
+            for x in self.seven_cards:
+                if x.suit == suit_return:
+                    self.flush_hand.append(x)
+
+        if self.four_of_kind == True:
+            print(self.firstname, 'Four of a kind : ', self.made_hand_four)
+
         if count_straight >= 5:
             print(self.firstname, 'has straight :', self.straight_list)
             self.straight = True
 
+        elif (self.three_of_kind and self.pair) == True:
+            print(self.firstname, 'full house : ', self.made_hand_three,
+                  "'s over ", self.made_hand, "'s")
+            self.full_house = True
+
+        elif self.three_of_kind == True:
+            print(self.firstname, 'Three of a kind : ', self.made_hand_three)
+
+        elif self.pair >= 2:
+            print(self.firstname, 'Two pairs of : ',
+                  sorted(self.made_hand[0:4], reverse=True))
+
+        elif self.pair == 1:
+            print(self.firstname, 'Pairs of : ', self.made_hand)
+
+        # Scoring
+
+        # Straight Flush - Error - counts straight flush from 7 not 5
+
+        # if (self.straight and self.flush) == True:
+        #     self.score += 80000 + self.straight_list[-1].value
+
+        # Full House
+
+        elif self.full_house == True:
+            self.test2.remove(self.made_hand_three[0])
+            self.test2.remove(self.made_hand_three[0])
+            self.test2.remove(self.made_hand_three[0])
+            self.made_hand_sorted = sorted(self.made_hand, reverse=True)
+            self.test2.remove(self.made_hand_sorted[0])
+            self.test2.remove(self.made_hand_sorted[0])
+            test2sorted = (sorted(self.test2, reverse=True))
+            self.score += 70000 + \
+                (self.made_hand_three[0]*500) + \
+                (self.made_hand_sorted[0])
+
+       # Four of a kind
+
+        elif self.four_of_kind == True:
+            self.test2.remove(self.made_hand_four[0])
+            self.test2.remove(self.made_hand_four[0])
+            self.test2.remove(self.made_hand_four[0])
+            self.test2.remove(self.made_hand_four[0])
+            test2sorted = (sorted(self.test2, reverse=True))
+            self.score += 60000 + \
+                (self.made_hand_four[0] * 500) + test2sorted[0]
+
+        # Flush
+
+        elif self.flush == True:
+            self.flush_hand_sorted = sorted(
+                self.flush_hand, key=attrgetter('value'), reverse=True)
+            print(self.flush_hand_sorted)
+            self.score += 50000 + (self.flush_hand_sorted[0].value*500) + \
+                (self.flush_hand_sorted[1].value*20) + self.flush_hand_sorted[2].value + \
+                ((self.flush_hand_sorted[3].value/20)) + (self.flush_hand_sorted[3].value /
+                                                          500) + (self.flush_hand_sorted[4].value/10000)
+
+       # Straight - Need to include wheel
+
+        elif self.straight == True:
+            self.score += 40000 + self.straight_list[-1].value
+            # TERMINOLOGY WORKS ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+       # Three of a kind
+
+        elif self.three_of_kind == True:
+            self.test2.remove(self.made_hand_three[0])
+            self.test2.remove(self.made_hand_three[0])
+            self.test2.remove(self.made_hand_three[0])
+            self.test2sorted = (sorted(self.test2, reverse=True))
+            self.score += 30000 + \
+                (self.made_hand_three[0] * 500) + \
+                (self.test2sorted[0]*20) + self.test2sorted[1]
+
+        # Two Pair
+
+        elif self.two_pair == True:
+            self.made_hand_sorted = sorted(self.made_hand, reverse=True)
+            self.test2.remove(self.made_hand_sorted[0])
+            self.test2.remove(self.made_hand_sorted[0])
+            self.test2.remove(self.made_hand_sorted[2])
+            self.test2.remove(self.made_hand_sorted[2])
+            self.test2sorted = (sorted(self.test2, reverse=True))
+            self.score += 20000 + \
+                (self.made_hand_sorted[0]*500) + \
+                (self.made_hand_sorted[3]*20) + self.test2sorted[0]
+
+        # 1 Pair
+
+        elif self.pair == 1:
+            self.made_hand_sorted = sorted(self.made_hand, reverse=True)
+            self.test2.remove(self.made_hand_sorted[0])
+            self.test2.remove(self.made_hand_sorted[0])
+            self.test2sorted = (sorted(self.test2, reverse=True))
+            self.score += 10000 + \
+                (self.made_hand_sorted[0]*500) + \
+                (self.test2sorted[0]*20) + self.test2sorted[1] + \
+                ((self.test2sorted[2]/20))
+
+        # High Card
+
+        else:
+            self.test2sorted = (sorted(self.test2, reverse=True))
+            self.score += (self.test2sorted[0]*500) + \
+                (self.test2sorted[1]*20) + self.test2sorted[2] + \
+                ((self.test2sorted[3]/20)) + (self.test2sorted[3] /
+                                              500) + (self.test2sorted[4]/10000)
+
+    # ========================= ===================== ============================
+
     # Check or Raise
 
-    def raise_(self):
-        print(self.chips)
-        print(self.firstname)
-        self.raise_amount = input('enter raise amount, or enter to check : ')
-        if self.raise_amount.isdigit() == True:
-            while True:
-                if int(self.raise_amount) <= self.chips:
-                    self.chips += - int(self.raise_amount)
-                    return int(self.raise_amount)
-                else:
-                    print('Not enough chips.')
-                    print(self.firstname)
-                    self.raise_amount = input(
-                        'enter raise amount, or enter to check : ')
-        else:
-            return 0
+    # Raising system - redo
 
-    # Call, Raise, Fold
+    # def raise_(self):
+    #     print(self.chips)
+    #     print(self.firstname)
+    #     self.raise_amount = input('enter raise amount, or enter to check : ')
+    #     if self.raise_amount.isdigit() == True:
+    #         while True:
+    #             if int(self.raise_amount) <= self.chips:
+    #                 self.chips += - int(self.raise_amount)
+    #                 return int(self.raise_amount)
+    #             else:
+    #                 print('Not enough chips.')
+    #                 print(self.firstname)
+    #                 self.raise_amount = input(
+    #                     'enter raise amount, or enter to check : ')
 
-    def crf(self):
-        self.user_decision = input('Type: Call, Raise, or Fold) ')
-        if self.user_decision.lower() == 'call':
-            self.in_hand = True
-            return True
-
-        elif self.user_decision.lower() == 'fold':
-            self.in_hand = False
-            return False
-
-        elif self.user_decision.lower() == 'raise':
-            self.in_hand = True
-            self.reraise_amount = input(
-                'enter raise amount, or enter to check : ')
-            if self.reraise_amount.isdigit() == True:
-                while True:
-                    if int(self.reraise_amount) <= self.chips:
-                        self.chips += - int(self.reraise_amount)
-                        return int(self.reraise_amount)
-                    else:
-                        print('Not enough chips.')
-                        self.reraise_amount = input(
-                            'enter raise amount, or enter to check : ')
-
-        else:
-            print(self.firstname, 'checked.')
+    # Give cardsd
 
     def give__cards(self):
         if self.card1:
@@ -276,7 +368,6 @@ class Player(object):
 
 
 # Give cards functions
-
 
 # Player inputs, put in afterwards, stored at end
 
@@ -302,6 +393,8 @@ class Game(object):
 
     # Community card functions
 
+    # Deal Flop
+
     def start_flop(self):
         x = deck.cards.pop()
         self.flop.append(x)
@@ -312,10 +405,14 @@ class Game(object):
         print('Flop : ', self.flop[0], '//',
               self.flop[1], '//', self.flop[2], '//', '\n')
 
+    # Deal Turn
+
     def start_turn(self):
         x = deck.cards.pop()
         self.flop.append(x)
         print('Turn : ', self.flop[3], '\n')
+
+    # Deal River
 
     def start_river(self):
         x = deck.cards.pop()
@@ -323,7 +420,8 @@ class Game(object):
         print('River : ', self.flop[4], '\n')
 
 
-# Game details and player information
+# Game details and player information - Fixed at present
+
 p1 = Player('Jack', 1000, [],)
 p2 = Player('Lolo', 1000, [],)
 p3 = Player('', 0, [], )
@@ -340,13 +438,14 @@ deck = StandardDeck()
 # ///////////////////////////////////////////////////////////////////////////////////
 
 # Key functions
-# reset()
-# give__cards(self)
-# self.reveal()
-# self.clear_hand()
-# start_flop()
-# start_turn()
-# start_river()
+# deck.reset()
+# p1.give__cards()
+# p1.reveal()
+# p1.clear_hand()
+# game.start_flop()
+# game.start_turn()
+# game.start_river()
+# p1.hand_ranking
 
 
 deck.reset()
@@ -362,158 +461,28 @@ game.start_turn()
 game.start_river()
 
 
-p1.flush_suit_checker()
-p2.flush_suit_checker()
-
+print('--------------------------Showdown-------------------------\n')
 p1.hand_ranking()
 print()
 p2.hand_ranking()
-print()
-p1.hand_ranking_straight()
-print()
-p2.hand_ranking_straight()
+
+print('\n-------------------------------------------------------------\n')
+print(p1.firstname, 'Ranking Score : ', p1.score)
+print(p2.firstname, 'Ranking Score : ', p2.score)
 
 
-# p1flush = []
-
-# for x in p1.seven_cards:
-
-
-#     print(p1flush, '\n\n', p2flush)
-# else:
-#     print('no code')
-
-
-# clubs = sum(1 for x in seven_cards if x.suit == "Clubs")
-# hearts = sum(1 for x in seven_cards if x.suit == "Hearts")
-# diamonds = sum(1 for x in seven_cards if x.suit == "Diamonds")
-# spades = sum(1 for x in seven_cards if x.suit == "Spades")
-
-# if clubs >= 5:
-#     print('flush')
-# if hearts >= 5:
-#     print('flush')
-# if diamonds >= 5:
-#     print('flush')
-# if spades >= 5:
-#     print('flush')
-# Test code /////////////////////////////////////////////////////////////////////////
-
-# y = sorted(seven_cards, key=lambda x: x.value, reverse=True)
-
-# flush = print(seven_cards)
-
-# clubs = sum(1 for x in seven_cards if x.suit == "Clubs")
-# hearts = sum(1 for x in seven_cards if x.suit == "Hearts")
-# diamonds = sum(1 for x in seven_cards if x.suit == "Diamonds")
-# spades = sum(1 for x in seven_cards if x.suit == "Spades")
-
-#
-#  if clubs >= 5:
-#     print('flush')
-# if hearts >= 5:
-#     print('flush')
-# if diamonds >= 5:
-#     print('flush')
-# if spades >= 5:
-#     print('flush')
-
-# seven_cards(self) = self.card1 + game.flop
-
-# flush = False
-
-# c = Counter(getattr(x, 'suit') for x in seven_cards)
-# if (c['Clubs'] or c['Hearts'] or c['Diamonds'] or c['Spade']) >= 5:
-#     print('flush')
-#     flush = True
-# else:
-#     print('no flush')
-
-# d = Counter(getattr(x, 'value') for x in seven_cards)
-# print(d)
-
-sys.exit()
-
-
-# Test code /////////////////////////////////////////////////////////////////////////
-
-# Think about combining player cards into 1 list.
-
-if (int(p1.card1[0].value) + int(p1.card1[1].value)) >= (int(p2.card1[0].value) + int(p2.card1[1].value)):
-    print(p1.firstname, 'wins\n')
-elif (int(p1.card1[0].value) + int(p1.card1[1].value)) == (int(p2.card1[0].value) + int(p2.card1[1].value)):
-    print('Draw\n')
+if p1.score > p2.score:
+    print(p1.firstname, 'wins')
+elif p2.score > p1.score:
+    print(p2.firstname, 'wins')
 else:
-    print(p2.firstname, 'wins\n')
+    print('Draw')
 
-p1.clear_hand()
-p2.clear_hand()
-
-
-# Test code /////////////////////////////////////////////////////////////////////////
-
-
-count = 0
-for x in deck.cards:
-    count += 1
-    print(x, count)
 
 sys.exit()
 
-while True:
-    if int(p1.raise_()) >= 1:
-        pot += int(p1.raise_amount)
-        print(p1.firstname, 'raised :', p1.raise_amount, '\nPOT : ', pot)
 
-        # P2
-
-        print(p2.firstname, 'Enter : Call, Raise, or Fold')
-
-        p2.crf()
-
-        if p2.user_decision.lower() == 'call':
-            print(p2.firstname, 'called')
-            pot += int(p1.raise_amount)
-            p2.chips += -int(p1.raise_amount)
-            break
-
-        elif p2.user_decision.lower() == 'fold':
-            print('follld')
-            p1.chips += pot
-            pot = 0
-            break
-
-        elif p2.user_decision.lower() == 'raise':
-            p2.chips += -int(p1.raise_amount)
-            pot += int(p1.raise_amount)
-
-            if int(p2.raise_()) >= 1:
-                pot += int(p2.raise_amount)
-                print(p2.firstname, 'raised :',
-                      p2.raise_amount, '\nPOT : ', pot)
-
-            else:
-                print('folded')
-
-    else:
-        print(p1.firstname, 'checked')
-        if int(p2.raise_()) >= 1:
-            pot += int(p2.raise_amount)
-            print(p2.firstname, 'raised :', p2.raise_amount, '\nPOT : ', pot)
-
-        else:
-            print(p2.firstname, 'checked')
-            in_raise = 'False'
-            break
-
-print('betting over')
-
-
-print(p1.reveal())
-print()
-print(p2.reveal())
-print()
-print('Pot : ', pot)
+# Test code /////////////////////////////////////////////////////////////////////////
 
 
 p1 = Player(user_name(), int(user_chips()), [], [])
